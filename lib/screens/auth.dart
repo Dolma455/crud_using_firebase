4c0853c8bec7ec83fb8480e6d7cf69dc6d3e2f08
Dolma <dolma1918@gmail.com>
2023-11-28 20:13:28 +0545

    feat: image picker added

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,4 @@
+import 'package:crud_firestore/widgets/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -69,6 +70,7 @@ class _AuthScreenState extends State<AuthScreen> {
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: [
+                            if (!_isLogin) const UserImagePicker(),
                             TextFormField(
                               decoration: const InputDecoration(
                                 labelText: 'Email Address',
