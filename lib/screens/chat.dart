bfcdf01955aeaf0b08110873a3cb450c835e1d2d
Dolma <dolma1918@gmail.com>
2023-11-30 13:37:59 +0545

    feat: request pust notification and get address token

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,39 @@
 import 'package:crud_firestore/widgets/chat_messages.dart';
 import 'package:crud_firestore/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  // void setupPushNotification() async {
+  //   final fcm = FirebaseMessaging.instance;
+  //   await fcm.requestPermission();
+  //   final token=await fcm.getToken();
+  //   print(token); //you could send this token via (HTTP or firestore)
+  // }
+  void setupPushNotification() async {
+    final fcmToken = await FirebaseMessaging.instance.getToken(
+        vapidKey:
+            "BBbY-Vz4Gq-6QyN_qOB-hIb7dy15E961RYOkn0gHkhjN65rwmgErJkRxrM-wEDTHqFdfJ3DsPYVR9PoJkmhUIKQ");
+    FirebaseMessaging.instance.onTokenRefresh
+        .listen((fcmToken) {})
+        .onError((err) {
+      print("Cannot generate token");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,5 +60,3 @@ class ChatScreen extends StatelessWidget {
         ));
   }
 }
-
-
